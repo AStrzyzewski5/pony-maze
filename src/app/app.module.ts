f67689dcb50936d114f3aeb346dde611fe4df3f5
Andrzej Strzyżewski <astrzyzewski5@gmail.com>
2019-04-28 11:46:59 +0200

    Created basic components and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StartPageComponent } from './components/start-page/start-page.component';
+import { GamePageComponent } from './components/game-page/game-page.component';
+import { MazeService } from './services/maze.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartPageComponent,
+    GamePageComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    MazeService
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
